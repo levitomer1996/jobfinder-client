bd950f1a1494189a7955f7e78c72a301f6c15837
levit <levitomer1996@gmail.com>
2025-01-23 21:06:17 +0200

    Added Header shows logged user name

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { AppBar, Toolbar, IconButton, Typography } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import { AuthContext } from "../../Context/AuthContext";
 
 const Header = () => {
+  const { user, isLogged } = useContext(AuthContext);
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
+
   return (
     <AppBar position="static" sx={{ backgroundColor: "white", color: "black" }}>
       <Toolbar>
@@ -12,15 +18,19 @@ const Header = () => {
         </Typography>
 
         {/* Sign In Text with Special Font */}
-        <p
-          style={{
-            fontFamily: "'Roboto', sans-serif",
-            fontWeight: "bold",
-            marginRight: "20px",
-          }}
-        >
-          <a href="/signin">Sign In</a>
-        </p>
+        {isLogged ? (
+          <p>{user.name}</p>
+        ) : (
+          <p
+            style={{
+              fontFamily: "'Roboto', sans-serif",
+              fontWeight: "bold",
+              marginRight: "20px",
+            }}
+          >
+            <a href="/signin">Sign In</a>
+          </p>
+        )}
 
         {/* Hamburger Menu */}
         <IconButton edge="end" color="inherit" aria-label="menu">
